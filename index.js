6a56700345461e6633a18fede00fd6d94ee65f06
loceyashley <locey_ashley@columbusstate.edu>
2020-10-13 17:16:32 +0000

    setup files

---
+++ index.js
@@ -0,0 +1,25 @@
+//require express
+var express = require('express');
+//require body parser
+var bodyParser = require ("body-parser");
+//require node-fetch
+ var fetch = require('node-fetch');
+//create express object call express
+var app = express();
+//create port information
+const port = process.env.PORT || 3000;
+//tells application to use ejs for templates
+app.set('view engine', 'ejs');
+//make styles pulic
+app.use(express.static("Public"));
+//tell app to use body parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+//server setup 
+app.listen(port,function(){
+    console.log('listening on ' + port)
+});
